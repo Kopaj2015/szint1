1368d5c79d369a45b1ff341d85c50568590a57f8
vinczea <wattila123@hotmail.com>
2015-11-17 23:35:35 +0100

    Add database bugfix

---
+++ server/fixtures.js
@@ -0,0 +1,36 @@
+if (Events.find().count() == 0) {
+	Events.insert({
+		name: 'Intro',
+		time: new Date(),
+		winner: '1',
+		type: 'test-sport1'
+	});
+	
+	Events.insert({
+		name: 'Intro1',
+		time: new Date(),
+		winner: '2',
+		type: 'test-sport'
+	});
+	
+	Events.insert({
+		name: 'Intro2',
+		time: new Date(),
+		winner: '1',
+		type: 'test-sport4'
+	});
+
+}
+	
+if (Floors.find().count() == 0) {
+	Floors.insert({
+		name: 'firsrfloor',
+		score: 100
+	});
+	
+	Floors.insert({
+		name: 'scondfloor',
+		score: 10
+	});
+	
+}
