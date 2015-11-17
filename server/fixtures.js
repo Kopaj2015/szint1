df0b5d0e364c292633ebb615e50c27b2b5953ac1
vinczea <wattila123@hotmail.com>
2015-11-18 00:54:08 +0100

    Add admin inserting

--- server/fixtures.js
+++ server/fixtures.js
@@ -35,4 +35,17 @@ if (Floors.find().count() == 0) {
 	
 }
 
-Meteor.call('insertFloor', 'firstflooooooor', 100, 'dsafd');
+if (Meteor.users.find().count() == 0) {
+	var users=[
+   {email: "admin@admin.com", username: "admin", name: "admin", roles:['admin']}
+	];
+	_.each(users, function(user){
+    Accounts.createUser({
+        email: user.email,
+        password: "admin",
+        profile: {username: user.username},
+        profile: {name: user.name},
+        roles: user.roles
+    });
+});
+}
