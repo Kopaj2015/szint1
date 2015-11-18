de60020c9cf64a41e746f0fce2c251c55875c963
Elabro <elabrobyte@gmail.com>
2015-11-18 01:39:45 +0100

    Remove in user_add.js console.log

--- client/views/modules/user_add.js
+++ client/views/modules/user_add.js
@@ -1,19 +1,13 @@
 if(Meteor.isClient) {
     Meteor.subscribe()
-    console.log("user added");
     Template.useradd.events ({
         'submit form': function(event) {
             event.preventDefault();
             var Username = event.target.name.value;
-            console.log(Username);
             var Userscore = event.target.score.value;
-            console.log(Userscore);
             var Password = event.target.password.value;
-            console.log(Password);
-            console.log("user added");
 
             Meteor.call('insertFloor',Username,Userscore,Password );
-
         }
     });
 }
