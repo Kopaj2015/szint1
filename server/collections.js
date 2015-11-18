ff590573b47f3ce9c9ca96c76a00f1a1bc40fe36
vinczea <wattila123@hotmail.com>
2015-11-18 01:57:43 +0100

    Add event methods

--- server/collections.js
+++ server/collections.js
@@ -20,6 +20,23 @@ Meteor.methods({
 	
 	'modifyFloor': function(selectedFloor, modifiedScore, modfiedPassword) {
 		Floors.update(selectedFloor, {$set: {score : parseInt(modifiedScore), password: modfiedPassword}});
+	},
+	
+	'insertEvent': function(eventName, eventDate, winner, type) {
+		Events.insert({
+			name: eventName,
+			time: eventDate,
+			winner: winner,
+			type: type
+		});
+	},
+	
+	'removeEvent': function(selectedEvent) {
+		Events.remove(selectedEvent);
+	},
+	
+	'modifyEvent': function(selectedEvent, modifiedName, modifiedTime, modifiedType) {
+		Events.update(selectedEvent, {$set: {name: modifiedName, time: modifiedTime, type: modifiedType}});
 	}
 
 });
